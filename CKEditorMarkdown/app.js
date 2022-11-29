d2bc8c234257a9980558fc2cd4e1efea6c5146da
daniel <dfm@stil.dk>
2022-11-29 15:32:25 +0100

    add demo, prettier

--- CKEditorMarkdown/app.js
+++ CKEditorMarkdown/app.js
@@ -1,28 +1,32 @@
-// app.js
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor"
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials"
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph"
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold"
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic"
+import Markdown from "@ckeditor/ckeditor5-markdown-gfm/src/markdown"
+import Table from "@ckeditor/ckeditor5-table/src/table"
 
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown';
+const plugins = [Essentials, Paragraph, Bold, Italic, Markdown, Table]
 
-let editor;
+const toolbar = ["Bold", "Italic", "Table"]
 
-ClassicEditor
-    .create( document.querySelector( '#editor' ), {
-        plugins: [ Essentials, Paragraph, Bold, Italic, Markdown ],
-        toolbar: [ 'bold', 'italic' ]
-    } )
-    .then( newEditor => {
-        console.log( 'Editor was initialized', newEditor );
-        editor = newEditor;
-    } )
-    .catch( error => {
-        console.error( error.stack );
-    } );
+let editor
 
-document.querySelector( '#saveData' ).addEventListener( 'click', () => {
-    const editorData = editor.getData();
-    console.log(editorData);
-} );
+ClassicEditor.create(document.querySelector("#editor"), {
+  plugins: plugins,
+  toolbar: toolbar,
+  table: Table,
+})
+  .then((newEditor) => {
+    console.log("Editor was initialized", newEditor)
+    editor = newEditor
+  })
+  .catch((error) => {
+    console.error(error.stack)
+  })
+
+document.querySelector("#saveData").addEventListener("click", () => {
+  const editorData = editor.getData()
+  console.log(editorData)
+  document.querySelector("#markdown-output").value = editorData
+})
