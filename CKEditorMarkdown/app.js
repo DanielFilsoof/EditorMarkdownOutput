3ddc0a959a469836b8ec14596a170fa81fcb7bd1
danny <dfm@stil.dk>
2022-11-29 21:08:20 +0100

    add tables to toolbar

--- CKEditorMarkdown/app.js
+++ CKEditorMarkdown/app.js
@@ -1,32 +1,47 @@
-import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor"
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials"
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph"
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold"
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic"
-import Markdown from "@ckeditor/ckeditor5-markdown-gfm/src/markdown"
-import Table from "@ckeditor/ckeditor5-table/src/table"
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown'
+import Table from '@ckeditor/ckeditor5-table/src/table'
 
 const plugins = [Essentials, Paragraph, Bold, Italic, Markdown, Table]
 
-const toolbar = ["Bold", "Italic", "Table"]
+// find toolbar names for plugins in editor object like so: editor.plugins._context.ui.componentFactory._components.<entries>
+const toolbar = [
+	'Bold',
+	'Italic',
+	'|',
+	'insertTable',
+	'tableColumn',
+	'tableRow',
+	'|',
+	'undo',
+	'redo'
+]
 
 let editor
 
-ClassicEditor.create(document.querySelector("#editor"), {
-  plugins: plugins,
-  toolbar: toolbar,
-  table: Table,
+ClassicEditor.create(document.querySelector('#editor'), {
+	plugins: plugins,
+	toolbar: toolbar,
+	table: Table
 })
-  .then((newEditor) => {
-    console.log("Editor was initialized", newEditor)
-    editor = newEditor
-  })
-  .catch((error) => {
-    console.error(error.stack)
-  })
+	.then(newEditor => {
+		console.log('Editor was initialized', newEditor)
+		editor = newEditor
+		console.log(
+			'toobar names ',
+			editor.plugins._context.ui.componentFactory._components
+		)
+	})
+	.catch(error => {
+		console.error(error.stack)
+	})
 
-document.querySelector("#saveData").addEventListener("click", () => {
-  const editorData = editor.getData()
-  console.log(editorData)
-  document.querySelector("#markdown-output").value = editorData
+document.querySelector('#saveData').addEventListener('click', () => {
+	const editorData = editor.getData()
+	console.log(editorData)
+	document.querySelector('#markdown-output').value = editorData
 })
