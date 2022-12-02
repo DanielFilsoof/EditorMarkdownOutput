31ceaf1e04ac8a5c328d448f3a2af60c4f5cb4fc
daniel <dfm@stil.dk>
2022-12-02 10:02:29 +0100

    remove features that are not by default compatible with markdwon

--- CKEditorMarkdown/app.js
+++ CKEditorMarkdown/app.js
@@ -2,7 +2,6 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown';
 import Table from '@ckeditor/ckeditor5-table/src/table';
@@ -10,22 +9,14 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import { Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
 import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
-import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
 import { ImageInsert } from '@ckeditor/ckeditor5-image';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 
 const plugins = [
 	Essentials,
 	Paragraph,
 	Bold,
 	Italic,
-	Underline,
 	Markdown,
 	Table,
 	TableToolbar,
@@ -33,14 +24,7 @@ const plugins = [
 	List,
 	Image,
 	ImageInsert,
-	ImageToolbar,
-	ImageCaption,
-	ImageStyle,
-	ImageResize,
-	LinkImage,
-	Heading,
-	Alignment,
-	MediaEmbed
+	Heading
 ];
 
 // find toolbar names for plugins in editor object like so: editor.plugins._context.ui.componentFactory._components.<entries>
@@ -48,18 +32,14 @@ const toolbar = [
 	'Heading',
 	'Bold',
 	'Italic',
-	'underline',
 	'alignment',
 	'link',
 	'bulletedList',
 	'numberedList',
 	'|',
 	'insertTable',
-	// 'tableColumn',
-	// 'tableRow',
 	'|',
 	'insertImage',
-	'mediaEmbed',
 	'|',
 	'undo',
 	'redo'
@@ -73,24 +53,13 @@ ClassicEditor.create(document.querySelector('#editor'), {
 	table: {
 		defaultHeadings: { rows: 1 },
 		contentToolbar: ['tableColumn', 'tableRow']
-	},
-	image: {
-		toolbar: [
-			'imageStyle:block',
-			'imageStyle:side',
-			'|',
-			'toggleImageCaption',
-			'imageTextAlternative',
-			'|',
-			'linkImage'
-		]
 	}
 })
 	.then(newEditor => {
 		console.log('Editor was initialized', newEditor);
 		editor = newEditor;
 		console.log(
-			'toobar names ',
+			'toolbar names ',
 			editor.plugins._context.ui.componentFactory._components
 		);
 	})
@@ -98,6 +67,8 @@ ClassicEditor.create(document.querySelector('#editor'), {
 		console.error(error.stack);
 	});
 
+// Test that the html is parsed to markdown and the other way around
+// through local storage
 document.querySelector('#saveData').addEventListener('click', () => {
 	const editorData = editor.getData();
 	document.querySelector('#markdown-output').value = editorData;
