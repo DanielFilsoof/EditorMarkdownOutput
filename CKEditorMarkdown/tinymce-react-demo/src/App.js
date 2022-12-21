86918e241ebd4482f469926d14859a763ca6c942
daniel <dfm@stil.dk>
2022-12-21 10:47:20 +0100

    add markdown<->html parser with localstorage saving/loading to tinymce demo

--- CKEditorMarkdown/tinymce-react-demo/src/App.js
+++ CKEditorMarkdown/tinymce-react-demo/src/App.js
@@ -1,47 +1,73 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import { htmlToMarkdown, markdownToHtml } from './bidirectionalHtmlMdParser';
 
 export default function App() {
-  const editorRef = useRef(null);
+	const editorRef = useRef(null);
+	const textAreaRef = useRef(null);
+	const [editorContent, setEditorContent] = useState('');
 
-  const save = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-      // TODO parse til md -> display i UI textare -> gem i localstorage
-      document.getElementById('text')
-    }
-  };
+	const save = async () => {
+		if (editorRef.current) {
+			const editorContentInMd = await htmlToMarkdown(
+				editorRef.current.getContent()
+			);
+			window.localStorage.setItem('editorContent', editorContentInMd);
+			document.getElementById('text');
+			textAreaRef.current.innerText = editorContentInMd;
+		}
+	};
 
-  const load = () => {
-      // TODO load fra localstorage -> parse til html -> display i UI editor
-      return 'todo';
-  }
+	const load = async () => {
+		const editorContentMd = window.localStorage.getItem('editorContent');
+		const editorContentHtml = await markdownToHtml(editorContentMd);
 
-  return (
-      <>
-        <Editor
-            tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-            onInit={(evt, editor) => editorRef.current = editor}
-            initialValue='<p>This is the initial content of the editor.</p>'
-            init={{
-              height: 500,
-              menubar: false,
-              plugins: [
-                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
-              ],
-              toolbar: 'undo redo | blocks | ' +
-                  'bold italic | bullist numlist outdent indent | ' +
-                  'removeformat | help',
-              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-            }}
-        />
-        <button onClick={save}>Log editor content</button>
+		setEditorContent(editorContentHtml);
+	};
 
-          <textarea id='textareaOutputTinymce'>
+	return (
+		<>
+			<Editor
+				tinymceScriptSrc={
+					process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'
+				}
+				value={editorContent}
+				onInit={(evt, editor) => (editorRef.current = editor)}
+				initialValue="<p>This is the initial content of the editor.</p>"
+				init={{
+					height: 500,
+					menubar: false,
+					plugins: [
+						'advlist',
+						'autolink',
+						'lists',
+						'link',
+						'image',
+						'charmap',
+						'anchor',
+						'searchreplace',
+						'visualblocks',
+						'code',
+						'fullscreen',
+						'insertdatetime',
+						'media',
+						'table',
+						'preview',
+						'help',
+						'wordcount'
+					],
+					toolbar:
+						'undo redo | blocks | ' +
+						'bold italic | bullist numlist outdent indent | ' +
+						'removeformat | help',
+					content_style:
+						'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+				}}
+			/>
+			<button onClick={save}>Gem</button>
+			<button onClick={load}>Indlæs</button>
 
-          </textarea>
-      </>
-  );
+			<textarea id="textareaOutputTinymce" ref={textAreaRef} />
+		</>
+	);
 }
